4cf37c64021f63206849d54c8fa5cf907aa1d0b6
analking228 <kronos5525@yandex.ru>
2021-03-25 19:17:22 +0300

    mysql done

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!bin/sh
 
-#export MINIKUBE_HOME=/Users/cjani/goinfre
-#minikube start --vm-driver=virtualbox
-#eval $(minikube docker-env)
-#minikube addons enable metallb
+export MINIKUBE_HOME=/Users/cjani/goinfre
+minikube start --vm-driver=virtualbox
+eval $(minikube docker-env)
+minikube addons enable metallb
 
 # docker
 docker build -t nginx_image ./srcs/nginx/
 #docker build -t ftps_image ./srcs/ftps/
-#docker build -t mysql_image ./srcs/mysql/
+docker build -t mysql_image ./srcs/mysql/
 #docker build -t phpmyadmin_image ./srcs/phpmyadmin/
 #docker build -t wordpress_image ./srcs/wordpress/
 #docker build -t influx_image ./srcs/influx/
